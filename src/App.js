12e275140f31631717c3b0c9db8a9c3bfb0a39cb
2303A51291 <161432770+hithesh2005@users.noreply.github.com>
2026-06-18 13:51:23 +0530

    Add Home component and update routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import PriorityNotifications from "./pages/PriorityNotifications";
+
+function Home() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>Campus Notifications Dashboard</h1>
+
+      <a href="/priority">
+        <button>View Priority Notifications</button>
+      </a>
+    </div>
+  );
+}
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<PriorityNotifications />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/priority" element={<PriorityNotifications />} />
       </Routes>
     </BrowserRouter>
   );
